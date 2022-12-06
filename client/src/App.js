e74e0c33fd43873e23150c184a66b4519ed4492c
ItsKalfar <gundsagar02@gmail.com>
2022-12-06 17:31:20 +0530

    second commit

--- client/src/App.js
+++ client/src/App.js
@@ -14,7 +14,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React with Sagar
         </a>
       </header>
     </div>
