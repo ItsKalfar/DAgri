c3876ac5df7fb92308ce03ada042caedc2308f2a
ItsKalfar <gundsagar02@gmail.com>
2022-12-07 15:28:46 +0530

    smart contract added

---
+++ server/deploy/01-deploy-supplychain.js
@@ -0,0 +1,23 @@
+const {
+  networkConfig,
+  developmentChains,
+} = require("../hardhat-helper-config");
+const { ethers, run, network } = require("hardhat");
+const { verify } = require("../utils/Verify");
+require("dotenv").config();
+
+module.exports = async ({ getNamedAccounts, deployments }) => {
+  const { deploy, log } = deployments;
+  const { deployer } = await getNamedAccounts();
+  const chainId = network.config.chainId;
+
+  args = [];
+  log("1) Deploying Contract----------------");
+  const supplyChain = await deploy("SupplyChain", {
+    from: deployer,
+    args: args,
+    log: true,
+    waitConfirmtions: network.config.blockConfirmations || 1,
+  });
+  log(`1) Contract Deplyed at ${supplyChain.address}...............`);
+};
