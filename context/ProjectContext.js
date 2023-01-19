25d69a27c33dd32c0f933cab20af26fc7281ed62
ItsKalfar <gundsagar02@gmail.com>
2023-01-19 23:57:44 +0530

    Added toast to give info about how to interact.

--- context/ProjectContext.js
+++ context/ProjectContext.js
@@ -28,6 +28,58 @@ export const ProjectContextProvider = ({ children }) => {
   const [stateChanged, setStateChanged] = useState(false);
 
   /**
+   * Prompts user to give information about how to access and use the web application.
+   */
+
+  const firstToast = async () => {
+    toast.custom((t) => (
+      <div
+        className={`${
+          t.visible ? "animate-enter" : "animate-leave"
+        } max-w-md w-full bg-white shadow-lg rounded-lg pointer-events-auto flex ring-1 ring-black ring-opacity-5`}
+      >
+        <div className="flex-1 w-0 p-4">
+          <div className="flex items-start">
+            <div className="ml-3 flex-1">
+              <p className="text-sm font-medium text-gray-900">
+                Welcome to MyPlace
+              </p>
+              <p className="mt-1 text-sm text-gray-500">
+                To use MySpace, make sure you have{" "}
+                <Link
+                  href="https://metamask.io/"
+                  target="_blank"
+                  className="text-blue-600 cursor-pointer"
+                >
+                  Metamask
+                </Link>{" "}
+                installed. Make an account in Metamask Wallet. Then switch the
+                network from Ethereum Mainnet to Goerli Test network. After
+                that, fund that account with some test ETH using{" "}
+                <Link
+                  href="https://goerlifaucet.com/"
+                  target="_blank"
+                  className="text-blue-600 cursor-pointer"
+                >
+                  Goerli Faucet.
+                </Link>
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="flex border-l border-gray-200">
+          <button
+            onClick={() => toast.dismiss(t.id)}
+            className="w-full border border-transparent rounded-none rounded-r-lg p-4 flex items-center justify-center text-sm font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+          >
+            Close
+          </button>
+        </div>
+      </div>
+    ));
+  };
+
+  /**
    * Prompts user to connect their MetaMask wallet
    * @param {*} metamask Injected MetaMask code from the browser
    */
@@ -367,6 +419,9 @@ export const ProjectContextProvider = ({ children }) => {
     if (currentAccount) {
       getAllProducts();
       getDistributerInventory();
+    }
+    if (typeof currentAccount == null) {
+      firstToast();
     }
     checkUser();
   }, [currentAccount, stateChanged]);
